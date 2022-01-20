55bb8fadbda472bfef4115a7f68eefec3e4d780f
Y-miura-ta <yuta.miumiu.tachikoma@gmail.com>
2022-01-20 21:09:20 +0900

    signal使用しないバージョン

--- TimerIMU.py
+++ TimerIMU.py
@@ -1,3 +1,4 @@
+import threading
 import time
 import board
 import adafruit_bno055
@@ -5,6 +6,7 @@ import numpy as np
 import quaternion
 import matplotlib.pyplot as plt
 import signal
+from threading import Lock
 
 class timerIMU():
     def __init__(self, _dt):
@@ -15,27 +17,44 @@ class timerIMU():
         self.p = np.array([0.0, 0.0, 0.0])
         self.v = np.array([0.0, 0.0, 0.0])
         self.a = np.array([0.0, 0.0, 0.0])
-        self.alpha = 0.5 # 過去の値に係数をかけて積分値を安定させる
-        self.dt =  _dt
+        self.alpha = 0.8#0.5 # 過去の値に係数をかけて積分値を安定させる
+        self.dt = _dt
         self.real_dt = self.dt
         self.controll_time = time.time()
-        signal.signal(signal.SIGALRM, self.velIntegral)
+        
+        thread = threading.Thread(target=self.velIntegral)
+        # signal.signal(signal.SIGALRM, self.velIntegral)
         # IMUの値が安定するまで3秒待つ
-        signal.setitimer(signal.ITIMER_REAL, 3.0, self.dt)
+        time.sleep(3)
+        thread.start()
+        # signal.setitimer(signal.ITIMER_REAL, 3.0, self.dt)
+        #self.signalNum = signal.SIGINT
+        #print(self.signalNum)
     
-    def velIntegral(self, arg1, arg2):
-        time_now = time.time()
-        self.real_dt = time_now - self.controll_time
-        quat = self.sensor.quaternion
-        acc = self.sensor.linear_acceleration
-        Quat = np.quaternion(quat[0], quat[1], quat[2], quat[3])
-        Acc = np.array([acc[0], acc[1], acc[2]])
-        Rot = quaternion.as_rotation_matrix(Quat)
-        self.a = Rot@Acc
-        # 積分
-        self.v = self.v*self.alpha + self.a*self.real_dt
-        self.p = self.p + self.v*self.real_dt
-        self.controll_time = time_now
+    #def setEvent(self, arg1, arg2):
+
+
+
+    def velIntegral(self):#, arg1, arg2):
+        while(1):
+            time_now = time.time()
+            quat = self.sensor.quaternion
+            acc = self.sensor.linear_acceleration
+            #print("calc rotation")
+            Quat = np.quaternion(quat[0], quat[1], quat[2], quat[3])
+            Acc = np.array([acc[0], acc[1], acc[2]])
+            Rot = quaternion.as_rotation_matrix(Quat)
+            #signal.pthread_sigmask(signal.SIG_BLOCK, [self.process])
+            # 外部から呼ばれる変数にアクセスするので他の処理を停止
+            lock = Lock()
+            with lock:
+                self.real_dt = time_now - self.controll_time
+                self.a = Rot@Acc
+                # 積分
+                self.v = self.v*self.alpha + self.a*self.real_dt
+                self.p = self.p + self.v*self.real_dt
+                self.controll_time = time_now
+            time.sleep(self.dt)
 
     def pauseTimer(self):
         signal.pause()
@@ -59,21 +78,27 @@ def main():
     plt.xlabel("time [s]")
     plt.ylabel("value [-]")
 
-    tIMU = timerIMU(0.02)
+    tIMU = timerIMU(0.005)
+    #tIMU.thread.start()
 
-    for i in range(1000):
-        print("dt:{}, world_vel_x:{}, world_vel_y:{}, world_vel_z:{}".format(tIMU.dt, tIMU.v[0], tIMU.v[1], tIMU.v[2]))
+    for i in range(100000):
+        #signal.pthread_sigmask(signal.SIG_BLOCK, [tIMU.proccess])
+        a_IMU = [tIMU.a[0], tIMU.a[1], tIMU.a[2]]
+        v_IMU = [tIMU.v[0], tIMU.v[1], tIMU.v[2]]
+        p_IMU = [tIMU.p[0], tIMU.p[1], tIMU.p[2]]
+        real_dt = tIMU.real_dt
+        #signal.pthread_sigmask(signal.SIG_UNBLOCK, [tIMU.proccess])
+        print("dt:{}, world_vel_x:{}, world_vel_y:{}, world_vel_z:{}".format(real_dt, v_IMU[0], v_IMU[1], v_IMU[2]))
         print("------------------------------------------------------------------------------------------")
-        time.sleep(0.1)
-        
+        #time.sleep(0.005)
         x = np.append(x, t)
         x = np.delete(x, 0)
         t = t + dt
-        y1 = np.append(y1, tIMU.v[0]*1000)
+        y1 = np.append(y1, v_IMU[0]*1000)
         y1 = np.delete(y1, 0)
-        y2 = np.append(y2, tIMU.v[1]*1000)
+        y2 = np.append(y2, v_IMU[1]*1000)
         y2 = np.delete(y2, 0)
-        y3 = np.append(y3, tIMU.v[2]*1000)
+        y3 = np.append(y3, v_IMU[2]*1000)
         y3 = np.delete(y3, 0)
         line, = plt.plot(x, y1, color='blue')
         line, = plt.plot(x, y2, color='red')
@@ -82,7 +107,7 @@ def main():
         plt.draw()
         plt.pause(dt)
         
-    tIMU.pauseIMU()
+    tIMU.pauseTimer()
     
 if __name__ == '__main__':
     main()
