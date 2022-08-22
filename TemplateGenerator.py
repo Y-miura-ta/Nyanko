5842ebf2c1a0ec8eaafa5a28c08c7a3f856e142f
Y-miura-ta <yuta.miumiu.tachikoma@gmail.com>
2022-08-22 23:10:11 +0900

    動かなくなった

--- TemplateGenerator.py
+++ TemplateGenerator.py
@@ -9,32 +9,31 @@ from mpl_toolkits.mplot3d import Axes3D
 import IK
 
 def rad2XYZ(rad_array):
-    leg_list = [0, 1, 2, 3]
-    pos_xyz = np.array(list(map(IK.legSmartFK, rad_array, leg_list)))
+    pos_xyz = np.array(list(map(IK.legFK, rad_array)))
+    print("-----------")
+    print(pos_xyz)
     
-    return pos_xyz
+    return pos_xyz[:, 2] # 脚先以外の座標もはいっているので抽出
             
 def main():
     template_file = './simple_walk.csv'
     data_df = pd.read_csv(template_file)
     
-    # 1??????????????????????????
-    n = 80 # ?????
+    # 一周分の歩行データをタイムスタンプの列を無視して取得
+    n = 80 # データ個数
     data_array = data_df.values[0:n, 1:13]
     data_len = len(data_array)
     
-    # ??????
+    # データを整形
     data_array = np.reshape(data_array, (data_len, 4, 3))
-
-    # ??????array???
-    rad2XYZ(data_array[0])
+    
+    # 各脚先座標のarrayを作成
     XYZ_array = np.array(list(map(rad2XYZ, data_array)))
-    print(XYZ_array)
-
-    # ????2???????CSV???
+    
+    # データを2次元配列にしてCSVに保存
     XYZ_mat = np.reshape(XYZ_array, (data_len, 12))
-    print(XYZ_mat)
-    XYZ_template_file = './simple_walk_xyz.csv'
+    print(XYZ_mat[0])
+    XYZ_template_file = './test_simple_walk_xyz.csv'
     with open(XYZ_template_file, 'w') as f:
         writer = csv.writer(f, lineterminator='\n')
         item_name = [
@@ -46,11 +45,10 @@ def main():
         writer.writerow(item_name)
         writer.writerows(XYZ_mat)
 
-    # 3D????
+    # 3Dプロット
     fig = plt.figure()
     ax = fig.add_subplot(111, projection="3d")
-    ax.set_box_aspect((1,1,1))
-
+    
     plot_leg = 0
 
     ax.scatter(XYZ_array[:, plot_leg, 0], XYZ_array[:, plot_leg, 1], XYZ_array[:, plot_leg, 2])
